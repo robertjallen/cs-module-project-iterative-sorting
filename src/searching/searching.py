a4872bd166bddf42d5bccec8ce62512a94f42801
robertjallen <bobbidigi34@gmail.com>
2020-09-02 17:08:33 -0400

    add lin_search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,8 +1,14 @@
+arr1 = [6, 5, 4, 9, 5, 3, 4, 2]
+arr2 = []
+
+
 def linear_search(arr, target):
-    # Your code here
-
-
+    # Check index = to target
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
+
 
 
 # Write an iterative implementation of Binary Search
@@ -12,3 +18,13 @@ def binary_search(arr, target):
 
 
     return -1  # not found
+
+
+
+
+
+
+
+
+lin_search = linear_search(arr1, -5)
+print(lin_search)
