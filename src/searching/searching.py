48f209b637be33b6309a4890756bc5b4611b6b6c
robertjallen <bobbidigi34@gmail.com>
2020-09-02 17:43:32 -0400

    add binary_search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,3 +1,6 @@
+
+
+
 arr1 = [6, 5, 4, 9, 5, 3, 4, 2]
 arr2 = []
 
@@ -13,8 +16,21 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
 
-    # Your code here
+    while low <= high:
+        middle = (low + high) // 2
+        guess = arr[middle]
+        if guess == target:
+            return middle
+
+        if target > guess:
+            low = middle + 1
+
+        else:
+            high = middle - 1
+
 
 
     return -1  # not found
@@ -25,6 +41,9 @@ def binary_search(arr, target):
 
 
 
+# sorted_nums = sorted(nums)
+# bin_search = binary_search(sorted_nums, my_target)
 
 lin_search = linear_search(arr1, -5)
 print(lin_search)
+# print(bin_search)
